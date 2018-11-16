bad89d2c4087204579de98798c1e90adf386c892
d1Ng0 <diegotrazzi@gmail.com>
2018-11-16 12:06:16 -0800

    Basic usage for POSTING job and reading form DB

--- hybrid_server/job_scheduler.py
+++ hybrid_server/job_scheduler.py
@@ -1,30 +1,29 @@
 import requests
 
-"""
-The first thing a scheduler should do is to keep an updated list of jobs to dispatch
-Based on the number of jobs and available blades, the scheduler should know where to dispatch the new tasks
-"""
+class Scheduler():
+    """
+    BAREBONE WIP: The first thing a scheduler should do is to keep an updated list of jobs to dispatch
+    Then, based on the number of jobs and number of available blades, the scheduler should know where to dispatch the new tasks
+    """
+     
+    resp = requests.get('http://localhost:8000/api/jobs/') #temp address Connect to the server API
+    if resp.status_code != 200:
+        # This means something went wrong.
+        raise Exception('GET /jobs/ {}'.format(resp.status_code))
+    data = resp.json()
+    for obj in data['objects']:
+        print(obj)
 
-"""
-Connect to the server API 
-"""
-
-class APIError(Exception):
-    """An API Error Exception"""
-
-    def __init__(self, status):
-        self.status = status
-
-    def __str__(self):
-        return "APIError: status={}".format(self.status)
+    # check how many free procs there are on local renderfarm
+    # check if other (cloud) renderfarm is available
+    # submit job (submit class should be abstract, each renderfarm should have an implementation to make it simple to switch between rendering farm options)
 
 
-resp = requests.get('http://localhost:8000/api/') #temp address
-if resp.status_code != 200:
-    # This means something went wrong.
-    raise APIError('GET /jobs/ {}'.format(resp.status_code))
-for todo_item in resp.json():
-    print('{} {}'.format(todo_item['name'], todo_item['summary']))
+if __name__ == '__main__':
+    app = Scheduler()
+
+
+
 
 
 
--- notable_django/test_job.py
+++ notable_django/test_job.py
@@ -5,18 +5,30 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notable_django.settings')
 django.setup()
 from api.models import Jobs
+import json
+import requests
 
 """
-Simple script to generate a job in the DB.
+Simple script to generate a job POST on the scheduler DB. This job should be assembled with a UI. The UI should be agnostic to which render-farm interface the process will be executed on
 """
 
-job_id = str(uuid.uuid4()) # will implement proper UUID later in the model
-name = 'test job name'
-user = 'dtrazzi'
-cmd = 'mayabatch test cmd -f 1-100' 
+name = 'Test job 2'
+user = os.getlogin() 
 timestamp = str(datetime.datetime.now())  # should be a DATETIMEField
+job_id = uuid.uuid4() # the job id should be returned by the server once entered in the DB to avoid clashing
 
-new_job = Jobs(job_id=job_id, name=name, user=user, cmd=cmd, timestamp=timestamp)
+params = dict()
+params['scene_file'] = '/filer/maya/test_project/scenes/test.mb'
+params['out_path'] = '/filer/maya/test_project/images/'
+params['renderer'] = 'arnold'
+params['frange'] = '1-100'
+params['step'] = '1'
+params['chunk_size'] = '2'
+params['xres'] = '640'
+params['yres'] = '480'
+params['camera'] = 'camera1'
+
+new_job = Jobs(job_id=job_id, name=name, user=user, cmd=params, timestamp=timestamp)
 new_job.save()
 
-print("{}".format(Jobs.objects.all())) # pylint: disable=no-member
+print("{}".format(params))  # pylint: disable=no-member
